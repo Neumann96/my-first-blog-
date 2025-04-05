e6dfebe6ae74d5e943bc42f7c82266261d70798c
Kirill <karachevkir@yandex.ru>
2025-04-06 00:43:22 +0300

    update html and views

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,8 @@
+from django.db.models.signals import post_save
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
