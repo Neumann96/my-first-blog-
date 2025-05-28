1f7804060cceb34afae88fbe4134d89ed5597500
Kirill <karachevkir@yandex.ru>
2025-05-28 11:20:20 +0300

    add image and view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.shortcuts import render
-from .models import Post
+from .models import Post, kkexam
 from django.utils import timezone
 from django.shortcuts import render, get_object_or_404
 
@@ -13,3 +13,8 @@ def post_list(request):
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+def kkexam_list(request):
+    exams = kkexam.objects.filter(is_public=True)
+    return render(request, 'kkexam_list.html', {'exams': exams})
