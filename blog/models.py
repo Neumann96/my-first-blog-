da7c7888ea345f10c35f0c810765f32e2db514cd
Kirill <karachevkir@yandex.ru>
2025-05-28 11:38:05 +0300

    update view image

--- blog/models.py
+++ blog/models.py
@@ -25,7 +25,7 @@ class kkexam(models.Model):
     name = models.CharField(max_length=200, verbose_name="Название экзамена")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     exam_date = models.DateTimeField(verbose_name="Дата проведения экзамена")
-    image = models.ImageField(upload_to='exam_images/', verbose_name="Изображение задания")
+    image = models.ImageField(upload_to='exam_images/', verbose_name="Изображение задания", blank=True, null=True)
     users = models.ManyToManyField(User, related_name='exams', verbose_name="Пользователи")
     is_public = models.BooleanField(default=False, verbose_name="Опубликовано")
 
